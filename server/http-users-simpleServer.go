b458b0d9db0114ba866084bc1f2dba737f95a6a6
sdtsui <szehungdanieltsui@gmail.com>
2016-09-06 21:14:37 -0700

    [wip] Read, Create operations working

--- server/http-users-simpleServer.go
+++ server/http-users-simpleServer.go
@@ -3,101 +3,141 @@ package main
 import (
   "net/http"
   "fmt"
+  "strings"
+  "strconv"
   "encoding/json"
   "net/url"
+  // "io"
 )
 
 //NEXT:
-// Marshall slice, send JSON.
 // receive JSON
 // search a slice, return an element
-// 
 // delete from a slice
 // update an element in a slice
 // http://www.alexedwards.net/blog/golang-response-snippets#json
-type User struct {
+type UserSchema struct {
   Username string
   Password string
   Email string
 }
-type SavedUser struct {
-  id int
-  user User
+type User struct {
+  Id int `json:"id"`
+  User UserSchema `json:"user"`
 }
-var firstUser = User{
+var firstUserSchema = UserSchema{
   Username: "Daniel",
   Password: "12345",
   Email: "daniel@blacklight.io",
 }
-var firstSavedUser = SavedUser{
-  id: 0,
-  user: firstUser,
+var secondUserSchema = UserSchema{
+  Username: "Daniel2",
+  Password: "54321",
+  Email: "dan@blacklight.io",
 }
-// var Users = make([]User, 0)
 var Users []User
 var usersCount = 0
 
 func init() {
   fmt.Println("Initializing server...")
+  var NewUser1 = User{usersCount, firstUserSchema}
+  Users = append(Users, NewUser1)
   usersCount++
-  Users = append(Users, firstUser)
+
+  var NewUser2 = User{usersCount, secondUserSchema}
+  Users = append(Users, NewUser2)
+  usersCount++
+
   fmt.Println("Initial Users :", Users)
 }
 
 func handler(w http.ResponseWriter, request *http.Request) {
-  decoder := json.NewDecoder(request.Body)
-  fmt.Println("Method:", request.Method)
-
-  if request.Method == "GET" { //TODO: write all users
-    js, err := json.Marshal(Users)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    fmt.Println("SENDING JS: ", js)
-    w.Write(js)
-
-    return
-  }
-
-  if request.Method == "POST" { //TODO: create a new user, append
-
-  }
-
-  if request.Method == "PATCH" {//TODO: update an existing user in the slice
-
-  }
-
-  if request.Method == "DELETE" { //TODO: remove a user from the slice
-
-  }
-
-  fmt.Println("DZ____________________")
-
   u, err := url.Parse(request.RequestURI)
   if err != nil {
       panic(err)
   }
   fmt.Println("Host :", u.Host)
-  fmt.Println("Url :", u.Path)
+  fmt.Println("Path :", u.Path)
+
+  if request.Method == "GET" { //TODO: write all users
+
+    // var id = strings.Split(u.Path, "/")[2]
+    // n, err := strconv.Atoi(id)
+    var id = strings.Split(u.Path, "/")
+    if (len(id) < 3) { //doesn't have ID
+      // fmt.Println("SENDING USERS: ", Users)
+      js, err := json.Marshal(Users)
+      if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+      }
+      w.Header().Set("Content-Type", "application/json")
+      w.WriteHeader(http.StatusOK)
+      w.Write(js)
+
+      return
+    } else if len(id) == 3 { //has ID
+      requestedId, convError := strconv.Atoi(id[2])
+      if convError != nil {
+        http.Error(w, convError.Error(), http.StatusInternalServerError)
+        return
+      }
+      for i := 0; i < len(Users); i++ {
+        if (Users[i].Id == requestedId) {
+          //match
+          js, err := json.Marshal(Users[i])
+          if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+          }
+          w.Header().Set("Content-Type", "application/json")
+          w.WriteHeader(http.StatusOK)
+          w.Write(js)
+          return
+        }
+      }
+
+      //no match
+      fmt.Println("ID ", requestedId, "Not Found")
+      w.WriteHeader(http.StatusNotFound)
+      return
+    }
 
 
-  var t User
-  err = decoder.Decode(&t)
-  if err != nil {
-    panic(err)
+    return
   }
-  fmt.Println("Test Data : ", t.Username)
-  fmt.Println("Test Data : ", t.Password)
-  fmt.Println("Test Data : ", t.Email)
 
-  // io.WriteString(w, "Hello world!")
+  if request.Method == "POST" { //TODO: create a new user, append
+    // insert a new element
+    decoder := json.NewDecoder(request.Body)
+    var u UserSchema
+    err = decoder.Decode(&u)
+    if err != nil {
+      panic(err)
+    }
+    newUser := User{usersCount, u}
+    usersCount++
+    Users = append(Users, newUser)
+    w.WriteHeader(http.StatusCreated)
+
+    return
+  }
+
+  if request.Method == "PATCH" {//TODO: update an existing user in the slice
+    // update an element
+    return
+  }
+
+  if request.Method == "DELETE" { //TODO: remove a user from the slice
+    // remove an element
+    return
+  }
 }
 
 func main() {
   mux := http.NewServeMux()
-  mux.HandleFunc("/users", handler)
+  mux.HandleFunc("/", handler)
   fmt.Println("Listening...")
+  fmt.Println("Users...", Users)
   http.ListenAndServe(":8000", mux)
 }
